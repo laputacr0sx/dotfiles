3432d38e19c96192a9e70c48d5b2e9838eeec489
FELIX NG <shng@shoalter.com>
2024-11-18 09:45:54 +0800

    init

---
+++ nvim/lua/plugins/java/init.lua
@@ -0,0 +1,43 @@
+return {
+  "nvim-java/nvim-java",
+  config = false,
+  dependencies = {
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          jdtls = {
+            settings = {
+              java = {
+                configuration = {
+                  runtimes = {
+                    {
+                      name = "JavaSE-23",
+                      path = "/usr/local/sdkman/candidates/java/23-tem",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        setup = {
+          jdtls = function()
+            require("java").setup({
+              root_markers = {
+                "settings.gradle",
+                "settings.gradle.kts",
+                "pom.xml",
+                "build.gradle",
+                "mvnw",
+                "gradlew",
+                "build.gradle",
+                "build.gradle.kts",
+              },
+            })
+          end,
+        },
+      },
+    },
+  },
+}
